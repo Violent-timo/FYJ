6b549ed510ae8b570ac085b925eee17f1b0e91a5
timor <1143151488@qq.com>
2020-06-01 22:25:46 +0800

    首页

---
+++ src/script/index.js
@@ -0,0 +1,15 @@
+// //头部导航效果
+// !function(){
+//     const $myorder=$('.myorder');
+//     const $myordercon=$('.myorder .con ul');
+//     $myorder.hover(function(){
+//             $myordercon.css({
+//                 height:'auto',
+//                 border:'1px solid #e5e5e5',
+//                 borderTop:'none',
+//                 transition:'1s'
+//             })
+            
+       
+//     })
+// }()
