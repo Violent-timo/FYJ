69a1ded894d5662a5766a8b90745e79a73f07adc
timor <1143151488@qq.com>
2020-06-02 22:11:52 +0800

    首页0602

--- src/script/index.js
+++ src/script/index.js
@@ -1,15 +1,24 @@
-// //头部导航效果
-// !function(){
-//     const $myorder=$('.myorder');
-//     const $myordercon=$('.myorder .con ul');
-//     $myorder.hover(function(){
-//             $myordercon.css({
-//                 height:'auto',
-//                 border:'1px solid #e5e5e5',
-//                 borderTop:'none',
-//                 transition:'1s'
-//             })
-            
-       
-//     })
-// }()
+//live效果
+!function(){
+    const $liveimg=$('.firlive .live-special img');
+    function move(){
+        $liveimg.animate({
+            width:'160',
+            height:'160',
+            left:'-70'
+        },1800,'linear').animate({
+            top:'-100'
+        },2000,'linear').animate({
+            left:'0'
+        },1800,'linear').animate({
+            top:'0'
+        },2000,'linear').animate({
+            width:'90',
+            height:'59',
+        },1800,'linear')
+    };
+    move();
+    setInterval(function(){
+        move();
+   },11000)
+}()
